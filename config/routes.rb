be8fee27e2a85523b6400abada9a84dc7bfb5512
Stephanie Cheney <steph@scheneyphotography.com>
2020-05-17 14:39:52 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
+  resources :students, only: [:index, :show]
+
+  # get '/activate/student/:id', to: 'activate#student'
+  get '/students/:id/activate', to: 'students#activate', as: 'activate_student'
+  # visit activate_student_path(@student)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
